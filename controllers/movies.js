34a6ec3def4ffd9642c105760797637abcbadba5
ademten82 <ademten7@gmail.com>
2022-01-07 10:57:56 +0100

    added movies

---
+++ controllers/movies.js
@@ -0,0 +1,21 @@
+const { movies, newMovies, actionMovies } = require("../models/movies");
+
+// export
+exports.movies = (req, res) => {
+  res.send(movies);
+};
+
+exports.newMovies = (req, res) => {
+  res.send(newMovies);
+};
+exports.actionMovies = (req, res) => {
+  res.send(actionMovies);
+};
+
+exports.getMovie = (req, res) => {
+  const movieId = req.params.id;
+
+  const findMovie = movies.find((movie) => movie.id === movieId);
+  console.log(movies);
+  res.send(findMovie);
+};
--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-//we are creating server
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
-
-//please take whatever port our machine use
-//React use also 3000 on frontend
 const port = process.env.PORT;
+const {
+  movies,
+  actionMovies,
+  newMovies,
+  getMovie,
+} = require("./controllers/movies");
+//1) all movies route
 
-app.get("/movies", (req, res) => {
-  res.send("Movies!");
-});
+app.get("/movies", movies);
+
+//2) new movies route
+app.get("/new-movies", newMovies);
+
+//3) action movies route
+app.get("/action-movies", actionMovies);
+
+//4) find movie by id
+//params ==> /:id
+//queries ==>?id=1234
+
+app.get("/movie/:id", getMovie);
+
+/*
 app.get("/tv-shows", (req, res) => {
   res.send("Tv Shows!");
-});
+});*/
 
-//
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
